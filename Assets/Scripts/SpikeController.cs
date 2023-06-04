062aca1b9fcc41b56086827a56e760d1fc8f5147
Jagrit Vats <69034224+jagritvats@users.noreply.github.com>
2023-06-04 23:48:17 +0530

    Spike functionality added

--- Assets/Scripts/SpikeController.cs
+++ Assets/Scripts/SpikeController.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpikeController : MonoBehaviour
 {
-    public LevelOverManager lom;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //[SerializeField] GameManager gameManager;
+
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
         }
     }
